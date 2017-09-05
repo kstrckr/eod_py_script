a8761b18275cf0f419e3bb64e5a7cf2b67b25b2d
Kurt Strecker <ksphotopro@gmail.com>
2017-09-04 21:32:09 -0400

    tweak to fake_data

--- fake_input.py
+++ fake_input.py
@@ -5,7 +5,7 @@ import random
 num = 1
 
 for num in range(1,16):
-    time.sleep(random.randint(1, 2))
+    time.sleep(random.randint(1, 4)/3)
     print('{} Beep!'.format(num))
     num+=1
     sys.stdout.flush()
