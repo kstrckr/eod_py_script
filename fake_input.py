d0fdd7769c7d3b34a2057cc1a605b69f35ead7a7
Gilt Groupe Studios <GGS@iMac-8.local>
2017-09-06 14:40:14 -0400

    working mac progress bar

--- fake_input.py
+++ fake_input.py
@@ -4,8 +4,8 @@ import random
 
 num = 1
 
-for num in range(1,16):
-    time.sleep(random.randint(1, 4)/3)
+for num in range(1,251):
+    time.sleep(0.1)
     print('{} Beep!'.format(num))
     num+=1
     sys.stdout.flush()
--- progress_bar.py
+++ progress_bar.py
@@ -29,21 +29,20 @@ def print_progress_bar(input_complete_length, bar_display_length):
     python_call = ['python', 'fake_input.py']
     spinner = Spinner()
     current_progress = 0
-    proc = subprocess.Popen(python_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen(python_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
 
     while proc.poll() is None:
         line = proc.stdout.readline().decode()
         if line:
             current_progress += 1
-            percent_complete = int((current_progress/input_complete_length
-        ) * 100)
+            percent_complete = int(float(current_progress)/float(input_complete_length) * 100)
             progress_bar_now = math.floor((bar_display_length * 0.01) * percent_complete)
-            progress_bar_viz = '|' * int(progress_bar_now) + '_' * (bar_display_length-int(progress_bar_now))
+            progress_bar_viz = '\033[42m{}\033[00m{}\n'.format('|' * int(progress_bar_now), '_' * (bar_display_length - int(progress_bar_now))) * 3
+            #progress_bar_viz = '{}{}'.format('|' * current_progress, '_' * (bar_display_length - current_progress))
             spinner_viz = spinner.update_spinner()
-            final_output = '[{}]\n{}/{} {}%\n{}'.format(spinner_viz, current_progress, input_complete_length
-        , percent_complete, progress_bar_viz)
+            final_output = '[{}]\n{}/{} {}%\n{}'.format(spinner_viz, current_progress, input_complete_length, percent_complete, progress_bar_viz)
             clear_screen()
             print(final_output)
             #print("\033[37m{}\033[0m".format(line.strip()))
 
-print_progress_bar(15, 80)
+print_progress_bar(250, 80)
